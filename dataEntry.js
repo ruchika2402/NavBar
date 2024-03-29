6cb5d497d8459674814639689a004e67cc43ee39
ruchika2402 <130750004+ruchika2402@users.noreply.github.com>
2024-03-28 23:20:54 -0700

    update

--- dataEntry.js
+++ dataEntry.js
@@ -34,24 +34,26 @@ function newDataInserted(employeeData)
 	let tbody=document.getElementById("table-body");
 	let newRow=tbody.insertRow(0);
 
-	let cell0=newRow.insetCell(0);
+	let cell0=newRow.insertCell(0);
 	cell0.innerHTML=employeeData.employeeName;
 
-	let cell1=newRow.insetCell(1);
-	cell.innerHTML=employeeData.employeeId;
+	let cell1=newRow.insertCell(1);
+	cell1.innerHTML=employeeData.employeeId;
 
-	let cell2=newRow.insetCell(2);
+	let cell2=newRow.insertCell(2);
 	cell2.innerHTML=employeeData.employeeDesignation;
    
-    let cell3=newRow.insetCell(3);
+    let cell3=newRow.insertCell(3);
 	cell3.innerHTML=employeeData.employeeDepartment;
 
-	let cell4=newRow.insetCell(4);
+	let cell4=newRow.insertCell(4);
 	cell4.innerHTML=employeeData.employeeSalary;
 
-	let cell5=newRow.insetCell(5);
+	let cell5=newRow.insertCell(5);
 	cell5.innerHTML=employeeData.employeeAddress;
 
+      let cell6=newRow.insertCell(6
+      	);
 	cell6.innerHTML=`<button onclick="editEmployeeData(this)">Edit</button>,
                     <button onclick="deleteEmployeeData(this)">DELETE</button>`;
 }
@@ -76,16 +78,24 @@ selectedRow=null;
  }
  function updateRow(employeeData)
  {
- 	selectedRow.cells[0].innerHTML=employeeName.value;
- 	selectedRow.cells[1].innerHTML=employeeId.value;
- 	selectedRow.cells[2].innerHTML=employeeDesignation.value;
- 	selectedRow.cells[3].innerHTML=employeeDepartment.value;
- 	selectedRow.cells[4].innerHTML=employeeSalary.value;
-    selectedRow.cells[5].innerHTML=employeeAddress.value;
+ 	selectedRow.cells[0].innerHTML=employeeName;
+ 	selectedRow.cells[1].innerHTML=employeeId;
+ 	selectedRow.cells[2].innerHTML=employeeDesignation;
+ 	selectedRow.cells[3].innerHTML=employeeDepartment;
+ 	selectedRow.cells[4].innerHTML=employeeSalary;
+    selectedRow.cells[5].innerHTML=employeeAddress;
 
  }
 function displayData(){
 	let tbody=document.getElementById("table-body");
 	let  newRow=tbody.insertRow(0);
 	let cell0=newRow.insetCell(0); 
+}
+
+function deleteEmployeeData(obj)
+{
+	let row =obj.parentElement.parentElement;
+	if(confirm()){
+		document.getElementById("table-body").deleteRow(row.index);
+	}
 }
